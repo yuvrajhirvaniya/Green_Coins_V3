1354d7392d457829fde553c5c86e9010d827b305
yuvrajhirvaniya <yuvrajhirvaniya23@gmail.com>
2025-06-02 14:43:49 +0530

    db

--- database/green_coins.sql
+++ database/green_coins.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2025 at 06:27 PM
+-- Generation Time: Jun 02, 2025 at 11:00 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
